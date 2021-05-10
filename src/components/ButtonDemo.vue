82520e13bf67bc08d74540582df2c58356fbd17d
Richard <shaodeyu@foxmail.com>
2021-05-10 19:28:27 +0800

    支持size 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,8 +1,21 @@
 <template>
-  <Button>button</Button>
-  <Button theme="button">button</Button>
-  <Button theme="link">button</Button>
-  <Button theme="text">button</Button>
+  <h3>示例1</h3>
+  <br />
+  <div>
+    <Button theme="button">button</Button>
+    <Button theme="link">button</Button>
+    <Button theme="text">button</Button>
+  </div>
+  <br />
+  <hr />
+  <br />
+  <h3>示例2</h3>
+  <br />
+  <div>
+    <Button size="big">大大大</Button>
+    <Button>普普通</Button>
+    <Button size="small">小小小</Button>
+  </div>
 </template>
 
 <script lang="ts">
