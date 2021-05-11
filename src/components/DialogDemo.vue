f04513977b7755da806fe83d30d2a0861cee4799
Richard <shaodeyu@foxmail.com>
2021-05-11 10:38:45 +0800

    创建Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>Dialog</div>
+  <div>
+    <div>Dialog</div>
+    <Dialog></Dialog>
+  </div>
 </template>
+
+<script lang="ts">
+import Dialog from "../lib/Dialog.vue";
+export default {
+  components: {
+    Dialog,
+  },
+};
+</script>
