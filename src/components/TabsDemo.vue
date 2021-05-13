372217596f24379701810de7b2428d0857cbcd02
Richard <shaodeyu@foxmail.com>
2021-05-14 01:28:38 +0800

    创建Tabs 和 Tab 组件

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>Tabs</div>
+  <Excode subheading="Tabs">
+    <Tabs>
+      <Tab title="导航1">内容1</Tab>
+      <Tab title="导航2">内容2</Tab>
+    </Tabs>
+  </Excode>
 </template>
+
+<script lang="ts">
+import Excode from "./Excode.vue";
+import Tabs from "../lib/Tabs.vue";
+import Tab from "../lib/Tab.vue";
+export default {
+  components: {
+    Tabs,
+    Tab,
+    Excode,
+  },
+};
+</script>
---
+++ src/lib/Tabs.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Tabs</h1>
+  </div>
+</template>
